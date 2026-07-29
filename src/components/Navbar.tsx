f1fcb5803dcf5e76493969db130946e7552562e0
Cristian-Fyranova <cristian.velasquez@fyranova.com>
2026-07-29 17:39:19 -0500

    fix: navbar routing fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,11 @@ export function Navbar() {
     <nav>
       <a href="#hero" className="logo">cv<span>.</span></a>
       <div className="nav-links">
-        <a href="#stack">Stack</a><a href="#projects">Projects</a><a href="#experience">Experience</a><a href="#chat">Ask me</a><a href="#contact">Contact</a>
+        <a href="#stack">Stack</a>
+        {/* <a href="#projects">Projects</a> */}
+        <a href="#experience">Experience</a>
+        <a href="#chat">Ask me</a>
+        <a href="#contact">Contact</a>
       </div>
     </nav>
   </>;
